c4f42b06603c99f117a1d036df932a1c4ae85c49
you@example.com <you@example.com>
2025-06-19 06:12:39 -0700

    staging to NAT

--- cnry-aws-spoke-deployment/landingzone.tf
+++ cnry-aws-spoke-deployment/landingzone.tf
@@ -20,24 +20,27 @@
 module "vpc1" {
   source  = "./modules/vpc-creation-protera"
 
-  #Basic info
+#Basic info
 
   name            = "bene-vpc-1"
   cidr            = "10.160.10.0/23"
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.160.10.0/25", "10.160.11.0/25"]
   public_subnets  = ["10.160.10.128/26", "10.160.11.128/26"]
-  staging_subnets = ["10.160.10.192/27"] #Uncomment if DRS/AMS is needed
 
   private_subnet_names = ["Private-AZ1", "Private-AZ2"]
   public_subnet_names = ["Public-AZ1", "Public-AZ2"]
+
+
+#Staging Subnet Configuration
+  staging_subnets = ["10.160.10.192/27"] #Uncomment if DRS/AMS is needed
   staging_subnet_names = ["Staging-Subnet-AZ1-PRV"] #Uncomment if DRS/AMS is needed
-  create_staging_subnet_route_table = true #Uncomment if DRS/AMS is neede
+  create_staging_subnet_route_table = true
+  create_staging_nat_gateway_route = true
 
 #NAT GW Configuration
   enable_nat_gateway = true
   single_nat_gateway = true
-  create_staging_nat_gateway_route = true
 
 #IGW COnfiguration
   create_igw = true
