aa898dbbac02ac4be4ebf55126d1984e02920fb5
you@example.com <you@example.com>
2025-06-19 05:57:56 -0700

    test staging with no TGW

--- cnry-aws-spoke-deployment/landingzone.tf
+++ cnry-aws-spoke-deployment/landingzone.tf
@@ -31,12 +31,13 @@ module "vpc1" {
 
   private_subnet_names = ["Private-AZ1", "Private-AZ2"]
   public_subnet_names = ["Public-AZ1", "Public-AZ2"]
-#   staging_subnet_names = ["Staging-Subnet-AZ1-PRV"] #Uncomment if DRS/AMS is needed
-#   create_staging_subnet_route_table = true #Uncomment if DRS/AMS is neede
+  staging_subnet_names = ["Staging-Subnet-AZ1-PRV"] #Uncomment if DRS/AMS is needed
+  create_staging_subnet_route_table = true #Uncomment if DRS/AMS is neede
 
 #NAT GW Configuration
   enable_nat_gateway = true
   single_nat_gateway = true
+  create_staging_nat_gateway_route = true
 
 #IGW COnfiguration
   create_igw = true
